3be11835ccb650d67668a18f6bfe81dee2e6779c
Patryk <p.bialek@kaft.pl>
2020-01-16 10:23:27 +0100

    add developer info

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'kaft-page';
+export class AppComponent implements OnInit{
+ 
+  ngOnInit() {
+    console.log("%cDear developer, welcome to our site.", "color: red; font-size: 20px")
+    console.log("%c-----------------------------------------------------", "color: red; font-size: 20px")
+    console.log("%cYou hit 'F12' by mistake, or you want to find something interesting?", "color: red; font-size: 20px")
+    console.log("%cIf you looking for a job, just contact us at job@kaft.pl", "color: red; font-size: 20px")
+  }
 }
